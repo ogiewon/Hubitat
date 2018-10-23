f3c4f64cf7ed75a2a9d212618c78371aaf49015b
ogiewon <danogorchock@yahoo.com>
2018-10-22 23:38:08 -0400

    v0.3.0 Added country selection

--- Alexa TTS/Apps/alexa-tts-manager.src/alexa-tts-manager.groovy
+++ Alexa TTS/Apps/alexa-tts-manager.src/alexa-tts-manager.groovy
@@ -1,5 +1,5 @@
 /**
- *  Alexa TTS Manager v0.2
+ *  Alexa TTS Manager
  *
  *  Copyright 2018 Daniel Ogorchock - Special thanks to Chuck Schwer for his tips and prodding me
  *                                    to not let this idea fall through the cracks!  
@@ -15,12 +15,13 @@
  *
  *  Change History:
  *
- *    Date        Who            What
- *    ----        ---            ----
- *    2018-10-20  Dan Ogorchock  Original Creation - with help from Chuck Schwer!
- *    2018-10-21  Dan Ogorchock  Trapped an error when invalid data returned from Amazon due to cookie issue.
- *    2018-10-21  Stephan Hackett Modified to include more Alexa Devices for selection
- *	
+ *    Version   Date        Who             What
+ *    -------   ----        ---             ----
+ *     v0.1.0   2018-10-20  Dan Ogorchock   Original Creation - with help from Chuck Schwer!
+ *     v0.1.1   2018-10-21  Dan Ogorchock   Trapped an error when invalid data returned from Amazon due to cookie issue
+ *     v0.2.0   2018-10-21  Stephan Hackett Modified to include more Alexa Devices for selection
+ *     v0.3.0   2018-10-22  Dan Ogorchock   Added support for Canada and United Kingdom, and ability to rename the app
+ *
  */
  
 definition(
@@ -32,13 +33,19 @@ definition(
     iconX2Url: "")
 
 preferences {
-    page(name: "pageOne", title: "Enter information from your alexa.amazon.com cookie file", nextPage: "pageTwo", uninstall: true) {
-        section("Enter your alexa.amazon.com 'cookie' file string here") {
+    page(name: "pageOne", title: "Alexa Cookie and Country selections", nextPage: "pageTwo", uninstall: true) {
+        section("Please Enter your alexa.amazon.com 'cookie' file string here (end with a semicolon)") {
             input("alexaCookie", "text", required: true)
-        }    
+        }
+        section("Please choose your country") {
+            input "alexaCountry", "enum", multiple: false, required: true, options: ["United States", "Canada", "United Kingdom"]
+        }
+        section("App Name") {
+            label title: "Optionally assign a custom name for this app", required: false
+        }
     }
-    page(name: "pageTwo", title: "Select your Amazon Alexa Devices", install: true, uninstall: true) {  
-        section("Select the Alexa Devices you would like to use") {
+    page(name: "pageTwo", title: "Amazon Alexa Device Selection", install: true, uninstall: true) {  
+        section("Please select devices to create Alexa TTS child devices for") {
             input "alexaDevices", "enum", multiple: true, required: false, options: getDevices()
         }  
     }
@@ -58,16 +65,15 @@ def speakMessage(String message, String device) {
             def SEQUENCECMD = "Alexa.Speak"
             def DEVICETYPE = "${it.deviceType}"
             def DEVICESERIALNUMBER = "${it.serialNumber}"
-            def LANGUAGE = "en-US"
             def MEDIAOWNERCUSTOMERID = "${it.deviceOwnerCustomerId}"
             def TTS= ",\\\"textToSpeak\\\":\\\"${message}\\\""
-            def command = "{\"behaviorId\":\"PREVIEW\",\"sequenceJson\":\"{\\\"@type\\\":\\\"com.amazon.alexa.behaviors.model.Sequence\\\",\\\"startNode\\\":{\\\"@type\\\":\\\"com.amazon.alexa.behaviors.model.OpaquePayloadOperationNode\\\",\\\"type\\\":\\\"${SEQUENCECMD}\\\",\\\"operationPayload\\\":{\\\"deviceType\\\":\\\"${DEVICETYPE}\\\",\\\"deviceSerialNumber\\\":\\\"${DEVICESERIALNUMBER}\\\",\\\"locale\\\":\\\"${LANGUAGE}\\\",\\\"customerId\\\":\\\"${MEDIAOWNERCUSTOMERID}\\\"${TTS}}}}\",\"status\":\"ENABLED\"}"
+            def command = "{\"behaviorId\":\"PREVIEW\",\"sequenceJson\":\"{\\\"@type\\\":\\\"com.amazon.alexa.behaviors.model.Sequence\\\",\\\"startNode\\\":{\\\"@type\\\":\\\"com.amazon.alexa.behaviors.model.OpaquePayloadOperationNode\\\",\\\"type\\\":\\\"${SEQUENCECMD}\\\",\\\"operationPayload\\\":{\\\"deviceType\\\":\\\"${DEVICETYPE}\\\",\\\"deviceSerialNumber\\\":\\\"${DEVICESERIALNUMBER}\\\",\\\"locale\\\":\\\"${getLanguage()}\\\",\\\"customerId\\\":\\\"${MEDIAOWNERCUSTOMERID}\\\"${TTS}}}}\",\"status\":\"ENABLED\"}"
         	def csrf = (alexaCookie =~ "csrf=(.*?);")[0][1]
             
-            def params = [uri: "https://pitangui.amazon.com/api/behaviors/preview",
+            def params = [uri: "https://${getAlexa()}/api/behaviors/preview",
                           headers: ["Cookie":"""${alexaCookie}""",
-                            "Referer": "https://alexa.amazon.com/spa/index.html",
-                            "Origin": "https://alexa.amazon.com",
+                            "Referer": "https://${getAmazon()}/spa/index.html",
+                                    "Origin": "https://${getAmazon()}",
                             "csrf": "${csrf}",
                             "Connection": "keep-alive",
                             "DNT":"1"],
@@ -99,30 +105,34 @@ def speakMessage(String message, String device) {
 }
 
 def getDevices() {
-    if (alexaCookie == null) {//log.debug "No cookie yet"
+    if (alexaCookie == null) {log.debug "No cookie yet"
                               return}
+    log.debug state.alexa
+    log.debug state.amazon
+    log.debug state.language
+    
     try{
         def csrf = (alexaCookie =~ "csrf=(.*?);")[0][1]
-        def params = [uri: "https://pitangui.amazon.com/api/devices-v2/device?cached=false",
+        def params = [uri: "https://${getAlexa()}/api/devices-v2/device?cached=false",
                       headers: ["Cookie":"""${alexaCookie}""",
-                      "Referer": "https://alexa.amazon.com/spa/index.html",
-                      "Origin": "https://alexa.amazon.com",
+                                "Referer": "https://${getAmazon()}/spa/index.html",
+                                "Origin": "https://${getAmazon()}",
                       "csrf": "${csrf}",
                       "Connection": "keep-alive",
                       "DNT":"1"],
                     requestContentType: "application/json; charset=UTF-8"
                   ]
  
-		httpGet(params) { resp ->
-        	//log.debug resp.contentType
-        	//log.debug resp.status
-    		//log.debug resp.data
-        	if ((resp.status == 200) && (resp.contentType == "application/json")) {
+       httpGet(params) { resp ->
+            //log.debug resp.contentType
+            //log.debug resp.status
+           //log.debug resp.data
+            if ((resp.status == 200) && (resp.contentType == "application/json")) {
                 def validDevices = []
                 atomicState.alexaJSON = resp.data
                 //log.debug state.alexaJSON.devices.accountName
                 atomicState.alexaJSON.devices.each {it->
-        	    if (it.deviceFamily in ["ECHO", "ROOK", "KNIGHT", "THIRD_PARTY_AVS_SONOS_BOOTLEG", "TABLET"]) {
+                    if (it.deviceFamily in ["ECHO", "ROOK", "KNIGHT", "THIRD_PARTY_AVS_SONOS_BOOTLEG", "TABLET"]) {
                         //log.debug "${it.accountName} is valid"
                         validDevices << it.accountName
                     }
@@ -130,12 +140,12 @@ def getDevices() {
                         validDevices << it.accountName
                     }
                 }
-            	return validDevices
-        	}
-        	else {
-            	log.error "Encountered an error. http resp.status = '${resp.status}'. http resp.contentType = '${resp.contentType}'. Should be '200' and 'application/json'. Check your cookie string!"
-            	return "error"
-        	}
+                return validDevices
+            }
+            else {
+                log.error "Encountered an error. http resp.status = '${resp.status}'. http resp.contentType = '${resp.contentType}'. Should be '200' and 'application/json'. Check your cookie string!"
+                return "error"
+            }
         }
     }
     catch (e) {
@@ -149,7 +159,7 @@ private void createChildDevice(String deviceName) {
     log.debug "'createChildDevice()': Creating Child Device '${deviceName}'"
         
     try {
-        def child = addChildDevice("ogiewon", "Child Alexa TTS", "${app.label}-${deviceName}", null, [name: "AlexaTTS-${deviceName}", label: "AlexaTTS ${deviceName}", completedSetup: true]) 
+        def child = addChildDevice("ogiewon", "Child Alexa TTS", "AlexaTTS${app.id}-${deviceName}", null, [name: "AlexaTTS-${deviceName}", label: "AlexaTTS ${deviceName}", completedSetup: true]) 
     } catch (e) {
        	log.error "Child device creation failed with error = ${e}"
     }
@@ -159,7 +169,6 @@ def installed() {
     log.debug "'installed()' called"
 	log.debug "'Installed' with settings: ${settings}"
     updated()
-    //subscribe()
 }
 
 def uninstalled() {
@@ -178,11 +187,11 @@ def updated() {
             def childDevice = null
             if(childDevices) {
                 childDevices.each {child->
-                    if (child.deviceNetworkId == "${app.label}-${alexaName}") {
+                    if (child.deviceNetworkId == "AlexaTTS${app.id}-${alexaName}") {
                         childDevice = child
                         //log.debug "Child ${app.label}-${alexaName} already exists"
                     }
-        	}
+                }
             }
             if (childDevice == null) {
                 createChildDevice(alexaName)
@@ -193,14 +202,58 @@ def updated() {
     catch (e) {
         log.error "Error in updated() routine, error = ${e}"
     }   
-    //unsubscribe()
-    //subscribe()
 }
 
-def subscribe() {
-    log.debug "'subscribe()' called"
+def getAlexa() {
+    switch (alexaCountry) {
+        case "United States": 
+                return "pitangui.amazon.com"
+            break
+        case "Canada": 
+                return "alexa.amazon.ca"
+            break
+        case "United Kingdom": 
+                return "layla.amazon.co.uk"
+            break
+        default: 
+            log.error "Unknown country = ${alexaCountry}"
+    }
+    return "error: Unknown country"
 }
 
+def getAmazon() {
+    switch (alexaCountry) {
+        case "United States": 
+                return "alexa.amazon.com" 
+            break
+        case "Canada": 
+                return "alexa.amazon.ca"
+             break
+        case "United Kingdom": 
+                return "amazon.co.uk" 
+           break
+        default: 
+            log.error "Unknown country = ${alexaCountry}"
+    }
+    return "error: Unknown country"
+}
+
+def getLanguage() {
+    switch (alexaCountry) {
+        case "United States": 
+                return "en-US"
+            break
+        case "Canada": 
+                return "en-US"
+            break
+        case "United Kingdom": 
+                return "en-GB"
+            break
+        default: 
+            log.error "Unknown country = ${alexaCountry}"
+    }
+    return "error: Unknown country"
+}
 
 def initialize() {
     log.debug "'initialize()' called"

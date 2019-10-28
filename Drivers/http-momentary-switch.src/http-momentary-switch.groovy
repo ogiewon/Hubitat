061bc5ce28e3ba98d5f55651b311983c698534c5
ogiewon <danogorchock@yahoo.com>
2019-10-28 08:30:18 -0400

    Added changes from stephack
    Support body and content type

--- Drivers/http-momentary-switch.src/http-momentary-switch.groovy
+++ Drivers/http-momentary-switch.src/http-momentary-switch.groovy
@@ -14,10 +14,10 @@
  *
  *  Change History:
  *
- *    Date        Who            What
- *    ----        ---            ----
- *    2018-02-18  Dan Ogorchock  Original Creation
- *
+ *    Date        Who             What
+ *    ----        ---             ----
+ *    2018-02-18  Dan Ogorchock   Original Creation
+ *    2019-10-11  Stephan Hackett Added ContentType and Body inputs(very limited functionality)
  * 
  */
 metadata {
@@ -31,11 +31,18 @@ metadata {
 		input(name: "devicePort", type: "string", title:"Device Port", description: "Enter Port of your HTTP server (defaults to 80)", defaultValue: "80", required: false, displayDuringSetup: true)
 		input(name: "devicePath", type: "string", title:"URL Path", description: "Rest of the URL, include forward slash.", displayDuringSetup: true)
 		input(name: "deviceMethod", type: "enum", title: "POST, GET, or PUT", options: ["POST","GET","PUT"], defaultValue: "POST", required: true, displayDuringSetup: true)
+		input(name: "deviceContent", type: "enum", title: "Content-Type", options: getCtype(), defaultValue: "application/x-www-form-urlencoded", required: true, displayDuringSetup: true)
+		input(name: "deviceBody", type: "string", title:"Body", description: "Body of message", displayDuringSetup: true)
 	}
 }
 
 def parse(String description) {
 	log.debug(description)
+}
+
+def getCtype() {
+    def cType = []
+    cType = ["application/x-www-form-urlencoded","application/json"]
 }
 
 def push() {
@@ -61,10 +68,11 @@ def off() {
 def runCmd(String varCommand, String method) {
 	def localDevicePort = (devicePort==null) ? "80" : devicePort
 	def path = varCommand 
-	def body = "" 
+	def body = ""
+	if(deviceBody) body = deviceBody
 	def headers = [:] 
     headers.put("HOST", "${deviceIP}:${localDevicePort}")
-	headers.put("Content-Type", "application/x-www-form-urlencoded")
+	headers.put("Content-Type", deviceContent)
 
 	try {
 		def hubAction = new hubitat.device.HubAction(
